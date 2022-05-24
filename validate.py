3cc9321a955790b0adde6e580db88bbf0096393d
Frank <franktrl2000@gmail.com>
2022-05-24 22:51:59 +0530

    Typo corrected in validate

--- validate.py
+++ validate.py
@@ -18,23 +18,23 @@ def word_equal(listed_word, guessed_word):
     if listed_word == guessed_word:
         return True
 
-def word_unequal(hidden_list, open_list, guessed_includes, guessd_right):
+def word_unequal(hidden_list, open_list, guessed_includes, guessed_right):
     temp_word = []
     right_letters = []
     for idx in range(MAX_LEN):
 
         letter = open_list[idx]
 
-        if letter in guessd_right and letter == hidden_list[idx]:
+        if letter in guessed_right and letter == hidden_list[idx]:
             temp_word.append(Fore.GREEN + letter + Fore.RESET)
-            guessd_right.remove(letter)
+            guessed_right.remove(letter)
             if letter in guessed_includes: guessed_includes.remove(letter)
             right_letters.append(letter)
 
         elif letter in guessed_includes:
             temp_word.append(Fore.YELLOW + letter + Fore.RESET)
             guessed_includes.remove(letter)
-            if letter in guessd_right: guessd_right.remove(letter)
+            if letter in guessed_right: guessed_right.remove(letter)
 
         else: temp_word.append(letter)
 
